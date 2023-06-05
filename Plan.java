a9e885ece2030b73e45e33ec1476ca33952dcb2d
jeffroyo <135653188+jeffroyo@users.noreply.github.com>
2023-06-05 15:04:47 -0400

    Created boiler plate

---
+++ Plan.java
@@ -0,0 +1,28 @@
+/* Plan class
+ * @description: Random file with program structure.
+ * @authors: Amun Ahmad & Jethro Lin
+ * @version: 6/5/2023
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display {
+  //
+}
+
+public class Draw {
+  //
+}
+
+public class Image {
+  //
+}
+
+public class Key {
+  //
+}
+
+public class Mouse {
+  //
+}
